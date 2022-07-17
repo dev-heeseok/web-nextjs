297a3b2de65b599fc21406cddebef59b72b66e7a
dev.heeseok <dev.heeseok@gmail.com>
2022-07-17 16:52:30 +0900

    improve: cookie header secure 추가

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,7 @@
 export default function loginRoute(req, res) {
   if (req.method === "POST") {
     console.log("로그인 실행");
-    res.setHeader("Set-Cookie", "a_name=Mike;Max-Age=3600;HttpOnly")
+    res.setHeader("Set-Cookie", "a_name=Mike;Max-Age=3600;HttpOnly,Secure")
     res.statusCode = 200;
     res.json({ message: "ok" });
   }
